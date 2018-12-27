07c7a954f9478664244271ab81ea95536a5ef0b3
AxelGarciaTello <gata21022000@hotmail.com>
2018-12-27 06:48:46 -0600

    base de datos en MySQL

---
+++ BD.sql
@@ -0,0 +1,4 @@
+create database pruebac;
+use pruebac;
+create table datos(nombre varchar(50), edad varchar(2), telefono varchar(10), correo varchar(50));
+insert into datos(nombre, edad, telefono, correo) values('juan','22','5555555555','juan@correo.com'),('carlos','34','9999999999','carlos@correo.com'),('maria','12','1111111111','maria@correo.com');
